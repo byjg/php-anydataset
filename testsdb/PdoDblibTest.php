321144dcfb03ca5ba75995886331a7352a5ef461
Joao Gilberto Magalhaes <joao@byjg.com.br>
2017-02-18 10:39:33 -0200

    Fix PdoDblibTest

---
+++ testsdb/PdoDblibTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Store;
+
+use ByJG\AnyDataset\Factory;
+
+require_once 'BasePdoTest.php';
+
+class PdoDblibTest extends BasePdoTest
+{
+
+    protected function createInstance()
+    {
+        $this->dbDriver = Factory::getDbRelationalInstance('dblib://sa:Pa$$word!@mssql-container/tempdb');
+    }
+
+    protected function createDatabase()
+    {
+        //create the database
+        $this->dbDriver->execute("CREATE TABLE Dogs (Id INT NOT NULL IDENTITY(1,1) PRIMARY KEY, Breed TEXT, Name TEXT, Age INTEGER)");
+    }
+
+    public function tearDown()
+    {
+        $this->dbDriver->execute('drop table Dogs;');
+    }
+}
